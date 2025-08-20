aaaa18fee76aad758b55e04b6cba8b126aa9ab9c
nolawit23 <eyerusalemnolawit@gmail.com>
2025-08-20 13:52:29 -0700

    Added recognizer and tts modules

---
+++ speech/recognizer.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+def listen():
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = recognizer.listen(source)
+    try:
+        return recognizer.recognize_google(audio).lower()
+    except sr.UnknownValueError:
+        return ""
